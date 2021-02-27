015d6daed688ce558f0425d7c8f775f5ebeed965
Alex Soto <asotobu@gmail.com>
2021-02-27 10:23:34 +0100

    Fixes test error

--- name-generator/src/test/java/org/acme/GreetingResourceTest.java
+++ name-generator/src/test/java/org/acme/GreetingResourceTest.java
@@ -1,21 +1,25 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class GreetingResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    @TestSecurity(authorizationEnabled = false)
+    public void testGenerateNameEndpoint() {
         given()
-          .when().get("/hello-resteasy")
+          .when().get("/generate/name")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body(anyOf(is("Ada"), is("Alexandra"), is("Burr"), is("Edson"), is("Kamesh"), is("Sebi"), is("Anna"), is("Gavina")));
     }
 
 }
